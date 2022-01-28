bcf11b348b3ea8b87b3f55ab07663e71f57e403a
favourlux <68516269+favourlux@users.noreply.github.com>
2022-01-28 16:01:30 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -20,6 +20,18 @@ app.get("/json", function(req, res) {
   });
 })
 
+app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({
+      "message": "HELLO JSON"
+    })
+  } else {
+    res.json({
+      "message": "Hello json"
+    })
+  }
+  
+})
 
 
 
