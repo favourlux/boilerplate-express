2d0faf071d8af1fee60fc2040ffb39e0512cbd08
favourlux <68516269+favourlux@users.noreply.github.com>
2022-01-28 14:15:18 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
-app.get("/", function(req, res) {
-  res.send("Hello Express");
+//app.get("/", function(req, res) {
+ // res.send("Hello Express");
+//})
+
+app.get("/", function(req, res) {  
+  var absolutePath = "/views/index.html";
+  res.sendFile(__dirname + absolutePath);
 })
 
 
